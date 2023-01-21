d501dbcd89c208acda1f2109500ad64e57437f49
Sayyam Ali <sayyamali267@gmail.com>
2023-01-21 23:42:59 +0500

    update

--- src/views/user/feeds/feeds.component.ts
+++ src/views/user/feeds/feeds.component.ts
@@ -3,7 +3,7 @@ import DrawerComponent from '@/views/drawer/drawer.component';
 import {Component} from 'vue-property-decorator';
 import { SocialMediaApi } from '@/sdk/api-services/social-media/social-media.api';
 import FollowersComponent from './followers/followers.component';
-import { SocialMediaService } from '@/sdk';
+import { PostModel, SocialMediaService } from '@/sdk';
 
 
 @Component({
@@ -34,8 +34,8 @@ export default class FeedComponent extends VueWrapper {
             .subscribe(
                 res => {
                     this.AlertSrv.show('success', 'post created successfully');
-
-                    new SocialMediaService().getTimelinePosts();
+                    this.SocialMediaSrv.timelinePosts = new Array<PostModel>();
+                    this.SocialMediaSrv.getTimelinePosts();
                     this.CoreSrv.CloseModal(this.model);
                     this.description = null;
                     this.media_file = null;
