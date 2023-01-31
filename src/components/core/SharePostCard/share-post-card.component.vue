21e1ea2e2bcfa9f5607817ceed9f45e755c8df55
Sayyam Ali <sayyamali267@gmail.com>
2023-01-31 21:05:15 +0500

    update

--- src/components/core/SharePostCard/share-post-card.component.vue
+++ src/components/core/SharePostCard/share-post-card.component.vue
@@ -23,7 +23,7 @@
                      <v-card color="darkgrey" rounded="lg" elevation="20" class="ma-auto border-card" width="600">
         
         <v-list  color="darkgrey" class="px-2" :rounded=true>
-            <v-list-item class="grow" @click="handlePost(item.post_id.id)">
+            <v-list-item class="grow" @click="$router.push({name: 'User Profile View', params: {id: item.post_id.creator.id}})">
                 <v-list-item-avatar size="60">
                     <img :src="item.post_id.creator.profile_pic" alt="John" />
                 </v-list-item-avatar>
@@ -41,7 +41,7 @@
             <span class="description text-start">{{ item.post_id.description }}</span>
         </v-row>
 
-        <v-avatar v-if="$helpers.isImageUrl(item.post_id.media_file)" size="100%" class="text-center pb-2" tile>
+        <v-avatar v-if="$helpers.isImageUrl(item.post_id.media_file)"  @click="handlePost(item.post_id.id)" size="100%" class="text-center cursor-pointer pb-2" tile>
             <img
                 :src="item.post_id.media_file"
                 alt="John"
@@ -53,7 +53,8 @@
             preload="auto"
             width="500"
             height="300"
-            
+            @click="handlePost(item.post_id.id)"
+            class="cursor-pointer"
             v-if="$helpers.isVideoUrl(item.post_id.media_file)"
         >
             <source
